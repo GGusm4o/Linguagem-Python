4cc60e154814e4bf757c86cf8334e7c1c130f252
GGusm4o <guilhermegusmao0811@gmail.com>
2025-08-26 17:09:56 -0400

    Estrutura de condições
    criação de um jogo de adivinhação

--- Desafios/Estrutura de condições/Desafio28.py
+++ Desafios/Estrutura de condições/Desafio28.py
@@ -1,4 +1,15 @@
+# Jogo de adivinhação
 from random import randint
-computador = randint(0, 5)
-print('Pensei no número {}'.format(computador)) # Faz o computador "PENSAR"
-jogador = int(input('Em que número eu pensei?'))
+from time import sleep
+
+computador = randint(0, 5) # Faz o computador "PENSAR"
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(3)
+if jogador == computador:
+    print('NÃO FEZ O MÍNIMO! Você conseguiu me vencer!')
+else:
+    print('Você errou KKKKK! Eu pensei no número {}.'.format(computador))
